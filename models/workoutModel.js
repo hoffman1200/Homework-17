2157bc211da6683a597b2e77ccf1c37a11c4ef79
hoffman1200 <hoffman1200@hotmail.com>
2020-07-30 16:10:29 -0500

    Routes Working

---
+++ models/workoutModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: 
+    }] 
+});
